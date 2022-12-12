0c8a4d53d27fe47a5262d57ac24b0cde60d0167e
Obscurely <adrian.obscurely@protonmail.com>
2022-12-12 20:00:14 +0200

    add homebrew formula

---
+++ macos/estash.rb
@@ -0,0 +1,11 @@
+class EStash < Formula
+    desc "An open source, cross-platform, programmed in rust, encrypted digital vault (store files and text) with the capability to set a path and with the click of a button to copy the contents to that file."
+    homepage "https://github.com/Obscurely/EStash"
+    url "https://github.com/Obscurely/EStash/releases/download/vVERSION_PLACEHOLDER/estash-macos.tar.gz"
+    sha256 "SHA_PLACEHOLDER"
+    version "VERSION_PLACEHOLDER"
+
+    def install
+        bin.install "estash"
+    end
+end

95d90ab5bb74ea52d5968e36e012530eaac0856c
Obscurely <adrian.obscurely@protonmail.com>
2022-12-12 19:49:14 +0200

    add macos files

---
+++ macos/make-dmg.sh
@@ -0,0 +1,11 @@
+create-dmg \
+    --volname EStash \
+    --volicon "estash.icns" \
+    --hide-extension "EStash.app" \
+    --background "dmg-background.png" \
+    --window-size 600 450 \
+    --icon-size 94 \
+    --icon "EStash.app" 141 249 \
+    --app-drop-link 458 249 \
+    ./estash-macos-installer.dmg \
+    ./EStash.app

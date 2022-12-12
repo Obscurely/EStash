d87248470c45974572664d2e1242a03e0bc866bd
Obscurely <59087558+Obscurely@users.noreply.github.com>
2022-12-12 20:16:07 +0200

    Update estash.rb

--- macos/estash.rb
+++ macos/estash.rb
@@ -1,7 +1,7 @@
 class Estash < Formula
     desc "An open source, cross-platform, programmed in rust, encrypted digital vault (store files and text) with the capability to set a path and with the click of a button to copy the contents to that file."
     homepage "https://github.com/Obscurely/EStash"
-    url "https://github.com/Obscurely/EStash/releases/download/vVERSION_PLACEHOLDER/estash-macos.tar.gz"
+    url "https://github.com/Obscurely/EStash/releases/download/vVERSION_PLACEHOLDER-stable/estash-macos.tar.gz"
     sha256 "SHA_PLACEHOLDER"
     version "VERSION_PLACEHOLDER"
 

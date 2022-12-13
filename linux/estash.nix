ea504467cd3a3d7eec943f24332a70c8cbf41d80
Obscurely <adrian.obscurely@protonmail.com>
2022-12-13 04:55:40 +0200

    add nixos files

---
+++ linux/estash.nix
@@ -0,0 +1,45 @@
+{ lib, stdenv, appimageTools, desktop-file-utils, fetchurl }:
+
+let
+  version = "VERSION_PLACEHOLDER";
+  name = "estash-${version}";
+
+  plat = {
+    x86_64-linux = "";
+  }.${stdenv.hostPlatform.system};
+
+  sha256 = {
+    x86_64-linux = "SHA_PLACEHOLDER";
+  }.${stdenv.hostPlatform.system};
+
+  src = fetchurl {
+    url = "https://github.com/Obscurely/EStash/releases/download/v${version}-stable/estash-linux.AppImage";
+    inherit sha256;
+  };
+
+  appimageContents = appimageTools.extractType2 {
+    inherit name src;
+  };
+in
+appimageTools.wrapType2 rec {
+  inherit name src;
+
+  extraInstallCommands = ''
+    mkdir -p $out/share/pixmaps $out/share/licenses/estash
+    cp ${appimageContents}/estash.png $out/share/pixmaps/
+    cp ${appimageContents}/EStash.desktop $out
+    cp ${appimageContents}/LICENSE $out/share/licenses/estash/LICENSE
+    mv $out/bin/${name} $out/bin/estash
+    ${desktop-file-utils}/bin/desktop-file-install --dir $out/share/applications \
+      --set-key Exec --set-value $out/bin/estash \
+      --set-key Comment --set-value "EStash Linux" \
+      --delete-original $out/EStash.desktop
+  '';
+
+  meta = {
+    homepage = "https://github.com/Obscurely/EStash";
+    description = "An open source, cross-platform, programmed in rust, encrypted digital vault (store files and text) with the capability to set a path and with the click of a button to copy the contents to that file";
+    license = lib.licenses.gpl3Only;
+    platforms = lib.platforms.linux;
+  };
+}
